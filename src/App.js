47fd454b8f1798c89edc5c5f6a75e1116da78d50
pawansarap <pawansarap519@gmail.com>
2024-05-03 23:17:39 +0530

    Created the static UI of the site

--- src/App.js
+++ src/App.js
@@ -1,3 +1,27 @@
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
 export default function App(){
-  return <div>hello react</div>
+  const step = 1;
+  function handlePrevious(){
+    alert("previours")
+  }
+  function handleNext(){
+    alert("next")
+  }
+  return <div className="steps">
+    <div className="numbers">
+      <div className={`${step >= 1 ? 'active':''}`}>1</div>
+      <div className={`${step >= 2 ? 'active':''}`}>2</div>
+      <div className={`${step >= 3 ? 'active':''}`}>3</div>
+    </div>
+    <p className="message">Step {step}: {messages [step-1]}</p>
+    <div className="buttons">
+    <button style={{backgroundColor: "#7950f2", color:"#ffffff"}} onClick={handlePrevious} >Previous</button>
+    <button style={{backgroundColor: "#7950f2", color:"#ffffff"}} onClick={handleNext} >Next</button>
+    </div>
+  </div>
 }

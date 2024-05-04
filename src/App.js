7a46ed09f3e372f1594690f5431819df7cb25d01
pawansarap <pawansarap519@gmail.com>
2024-05-04 23:16:49 +0530

    add the code to make the display none of the card when any one click on the close and same any one click on open it will show

--- src/App.js
+++ src/App.js
@@ -8,40 +8,47 @@ const messages = [
 
 export default function App() {
   const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
+
   // const [test, setTest] = useState({name: " Pawan"});
   function handleNext() {
-    if(step < 3) setStep(step + 1);
+    if (step < 3) setStep(step + 1);
     // setTest({name:" Keep Gooing Pawan"});
   }
   function handlePrevious() {
-    if(step >1 ) setStep(step - 1);
+    if (step > 1) setStep(step - 1);
     // setTest({name:" Try One More Time Pawan"});
   }
   return (
-    <div className="steps">
-      <div className="numbers">
-        <div className={step >= 1 ? "active" : ""}>1</div>
-        <div className={step >= 2 ? "active" : ""}>2</div>
-        <div className= {step >= 3 ? "active" : ""}>3</div>
-      </div>
-      <p className="message">
-        Step {step}: {messages[step - 1]}
-        {/* { test.name} */}
-      </p>
-      <div className="buttons">
-        <button
-          style={{ backgroundColor: "#7950f2", color: "#ffffff" }}
-          onClick={handlePrevious}
-        >
-          Previous
-        </button>
-        <button
-          style={{ backgroundColor: "#7950f2", color: "#ffffff" }}
-          onClick={handleNext}
-        >
-          Next
-        </button>
-      </div>
-    </div>
+    <>
+    <div  onClick={()=>setIsOpen(!isOpen)}>{isOpen ? <button className="close"> Close &times;</button>: <button className="close">Open +</button>} </div>
+      { isOpen &&
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
+          </div>
+          <p className="message">
+            Step {step}: {messages[step - 1]}
+            {/* { test.name} */}
+          </p>
+          <div className="buttons">
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#ffffff" }}
+              onClick={handlePrevious}
+            >
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#ffffff" }}
+              onClick={handleNext}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      }
+    </>
   );
 }

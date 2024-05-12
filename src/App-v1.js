755d006bfca343c8b73067e8125d4ad6ef8bcfdd
pawansarap <pawansarap519@gmail.com>
2024-05-12 22:28:43 +0530

    update the App.js file now this is the v2 of steps project

--- src/App-v1.js
+++ src/App-v1.js
@@ -13,21 +13,19 @@ export default function App() {
   // const [test, setTest] = useState({name: " Pawan"});
   function handleNext() {
     // if (step < 3) setStep(step + 1);
-    if (step < 3) setStep((s) => s + 1);
-    // if (step < 3) setStep((s)=> s + 1);  // increse the count by 2 by calling the call back funciton 2's
+    if (step < 3) setStep((s)=> s + 1);
+    // if (step < 3) setStep((s)=> s + 1);  // increse the count by 2 by calling the call back funciton 2's 
 
     // setTest({name:" Keep Gooing Pawan"});
   }
   function handlePrevious() {
-    if (step > 1) setStep((s) => s - 1);
+    if (step > 1) setStep((s)=> s - 1);
     // setTest({name:" Try One More Time Pawan"});
   }
   return (
     <>
       {/* <div onClick={() => setIsOpen(!isOpen)}> */}
-      <div onClick={() => setIsOpen((is) => !is)}>
-        {" "}
-        {/* above and this line doe the same thing but here we are calling call back funciton  */}
+      <div onClick={() => setIsOpen((is) => !is)}>  {/* above and this line doe the same thing but here we are calling call back funciton  */}
         {isOpen ? (
           <button className="close"> Close &times;</button>
         ) : (
@@ -46,27 +44,21 @@ export default function App() {
             {/* { test.name} */}
           </p>
           <div className="buttons">
-            <Button textColor="#fff" bgColor="#7950f2" onClick={handlePrevious}>
-              <span>👈</span> Previous
-            </Button>
-            <Button textColor="#fff" bgColor="#7950f2" onClick={handleNext}>
-              Next <span>👉</span>
-            </Button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#ffffff" }}
+              onClick={handlePrevious}
+            >
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#ffffff" }}
+              onClick={handleNext}
+            >
+              Next
+            </button>
           </div>
         </div>
       )}
     </>
-  );
-}
-
-// children is the pre define key word of react
-function Button({ textColor, bgColor, onClick, children }) {
-  return (
-    <button
-      style={{ backgroundColor: bgColor, color: textColor }}
-      onClick={onClick}
-    >
-      {children}
-    </button>
   );
 }
